3d8e85efafa8d69f1b589a0d7dacf01c1edddb4f
Tomasz Pietka <33056585+Tomasz-P@users.noreply.github.com>
2019-11-03 19:36:09 +0100

    Działający program w wersji 1.0 - ze wzrocowym plikiem konfiguracyjnym.

---
+++ DevicesDataBackupper_v1.0.py
@@ -0,0 +1,83 @@
+#! python3
+"""
+Pobieranie danych na temat konfiguracji z urządzeń, umieszczanie ich w plikach we wskazanym katalogu.
+Dane dotyczące urządzeń, ich adresacji, danych dostępowych, komend do wydania pobierane są z pliku konfiguracyjnego "config_file.ini"
+z wykorzystaniem modułu "configparser"-a.
+"""
+
+from devices_data_backupper import DeviceDataBackupper
+from pathlib import Path
+from os import path, scandir, rename
+import configparser
+import logging
+import re
+
+
+# FUNKCJE
+def add_numbersuffix_to_filename(full_filename):
+    """Dodanie suffix-u do nazwy pliku logów, gdy jego rozmiar przekroczy wartość logfile_size podaną w bajtach.
+    Zapis kolejnych logów odbywa się do kolejnego pliku bez suffix-u, nowo utworzonego.
+    """
+    filename = Path(full_filename).stem
+    filename_extension = Path(full_filename).suffix[1:]
+    numbers = []
+    fileRegex = re.compile('(^' + filename + '_)(\d{4})\.(' + filename_extension + '$)')
+    with scandir('.') as entries:
+        for entry in entries:
+            matched_fullfilename = re.search(fileRegex, entry.name)
+            if matched_fullfilename:
+                numbers.append(int(matched_fullfilename.group(2)))
+    numbers.sort()
+    next_number = (numbers[-1] + 1)
+    numbered_fullfilename = filename + (4 - len(str(next_number))) * '0' + str(next_number) + '.'\
+                            + filename_extension
+    rename(full_filename, numbered_fullfilename)
+    return 0
+
+def set_logging(logfilename, logging_level, logfile_maxsize):
+    '''Ustawienie poziomu logowania zdarzeń w trakcie wykonywania programu.'''
+    if Path(logfilename).exists() and path.getsize(logfilename) >= int(logfile_maxsize):
+        add_numbersuffix_to_filename(logfilename)
+    if logging_level == 'CRITICAL':
+        logging.basicConfig(filename=logfilename, filemode='a', level=logging.CRITICAL,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+    elif logging_level == 'ERROR':
+        logging.basicConfig(filename=logfilename, filemode='a', level=logging.ERROR,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+    elif logging_level == 'WARNING':
+        logging.basicConfig(filename=logfilename, filemode='a', level=logging.WARNING,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+    elif logging_level == 'INFO':
+        logging.basicConfig(filename=logfilename, filemode='a', level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+    else:
+        logging.basicConfig(filename=logfilename, filemode='a', level=logging.DEBUG,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info(f'Logging level set to {logging_level} for file {logfilename}.')
+    return 0
+
+def main():
+    """Funkcja główna."""
+    try:
+        print('Uruchomiono program.')
+        CONFIG_FILENAME = 'config_file_home.ini'
+        CFG_GENERAL_PARAMETERS = 'general_parameters'
+        CFG_LOGFILENAME = 'logfilename'
+        CFG_LOG_LEVEL = 'log_level'
+        CFG_LOGFILE_MAXSIZE = 'logfile_maxsize'
+
+        config = configparser.ConfigParser()
+        config.read(CONFIG_FILENAME)
+        set_logging(config[CFG_GENERAL_PARAMETERS][CFG_LOGFILENAME], config[CFG_GENERAL_PARAMETERS][CFG_LOG_LEVEL],
+                    config[CFG_GENERAL_PARAMETERS][CFG_LOGFILE_MAXSIZE])
+        backupper = DeviceDataBackupper(CONFIG_FILENAME)
+        backupper.backup_devices_data()
+        logging.info('The program has been completed')
+        print('Program zakończył działanie.')
+    except:
+        print('\nWYSTĄPIŁ BŁĄD W TRAKCIE WYKONYWANIA PROGRAMU!!!\nWięcej informacji w pliku logów.\n')
+        logging.error('An error has occurred! The program ended with an exception: ', exc_info=True)
+
+# PROGRAM GŁÓWNY
+if __name__ == "__main__":
+    main()
---
+++ devices_data_backupper.py
@@ -0,0 +1,192 @@
+"""
+Moduł umożliwiający obsługę urządzeń pod względem zrzutów ich konfiguracji.
+"""
+
+import configparser
+import paramiko
+from pathlib import Path
+from datetime import datetime
+import logging
+
+
+# KLASY
+class DeviceDataBackupper(object):
+    """Klasa do obsługi zrzucania danych konfiguracyjnych z urządzeń."""
+
+    def __init__(self, config_filename):
+        """Konstruktor klasy do obsługi zrzucania danych konfiguracyjnych z urządzeń."""
+        self.CONFIG_FILENAME = config_filename
+        self.__DEVICES_SECTION = 'devices'
+        self.__GENERAL_PARAMETERS = 'general_parameters'
+        self.__bkpfiles_directory = 'bkpfiles_directory'
+        self.__device_ipaddress = 'device_ip'
+        self.__device_port = 'port'
+        self.__device_username = 'user_name'
+        self.__device_password = 'password'
+        self.__device_timeout = 12
+
+    def backup_devices_data(self):
+        """Zbackupowanie danych z urządzeń do plików i zapis na dysku we wskazanym katalogu."""
+        devices_to_cmdsets_dict = self.get_devices_to_cmdsets_dict(self.CONFIG_FILENAME)
+        devices_list = self.get_devices_list_from_dict(devices_to_cmdsets_dict)
+        commandsets_list = self.get_commandsets_list_from_dict(devices_to_cmdsets_dict)
+        commandsets_dict = self.get_command_sets(self.CONFIG_FILENAME, commandsets_list)
+        for device_name in devices_list:
+            self.get_data_from_device_to_files(device_name, commandsets_dict[devices_to_cmdsets_dict[device_name]])
+
+    def get_devices_to_cmdsets_dict(self, config_filename):
+        """Pobranie zestawu urządzeń z pliku konfiguracyjnego i utworzenie słownika z wpisami
+        nazwa_urządzenia:zestaw_dla_urządzenia.
+        """
+        devices = {}
+        config = configparser.ConfigParser()
+        config.read(config_filename)
+        command_sets = config.options(self.__DEVICES_SECTION)
+        for command_set in command_sets:
+            dev_names = config[self.__DEVICES_SECTION][command_set].split('\n')[1:]
+            for dev_name in dev_names:
+                devices[dev_name] = command_set
+        logging.info(f'Dictionary created - devices to command sets: {devices}')
+        return devices
+
+    def get_devices_list_from_dict(self, dict):
+        """Wyodrębnienie listy urządzeń ze słownika."""
+        devices_list = []
+        for key in dict.keys():
+            devices_list.append(key)
+        logging.info(f'Devices list created: {devices_list}')
+        return devices_list
+
+    def get_commandsets_list_from_dict(self, dict):
+        """Wyodrębnienie listy zestawów komend ze słownika."""
+        commandsets_list = []
+        for key in dict.keys():
+            if dict[key] not in commandsets_list:
+                commandsets_list.append(dict[key])
+        logging.info(f'Command sets list created: {commandsets_list}')
+        return commandsets_list
+
+    def get_device_parameters(self, config_filename, device_name):
+        """Pobranie parametrów dla danego urządzenia z pliku konfiguracyjnego i przekształcenie do formy słownika."""
+        device_parameters = {}
+        config = configparser.ConfigParser()
+        config.read(config_filename)
+        for parameter in config.options(device_name):
+            device_parameters[parameter] = config[device_name][parameter]
+        logging.info(f'Parameters for device: {device_name} downloaded: {device_parameters}')
+        return device_parameters
+
+    def get_command_sets(self, config_filename, commandsets_list):
+        """Wyodrębnienie z pliku konfiguracyjnego zestawu komend z podziałem na urządzenia oraz pliki."""
+        commandsets_dict = {}
+        files_dict = {}
+        config = configparser.ConfigParser()
+        config.read(config_filename)
+        for commandset in commandsets_list:
+            for seperate_filename in config.options(commandset):
+                commands_list = config[commandset][seperate_filename].split('\n')[1:]
+                files_dict[seperate_filename] = commands_list
+            commandsets_dict[commandset] = files_dict
+            files_dict = {}
+        logging.info(f'Command sets dictionary created: {commandsets_dict}')
+        return commandsets_dict
+
+    def get_data_from_device_to_files(self, device, device_command_set):
+        """Pobranie danych z urządzenia do zbackup-owania."""
+        try:
+            print(f'Pobieranie danych dla urządzenia {device}...')
+            devconnection = paramiko.SSHClient()
+            devconnection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            device_parameters = self.get_device_parameters(self.CONFIG_FILENAME, device)
+            devconnection.connect(hostname=device_parameters[self.__device_ipaddress], port=device_parameters[self.__device_port],
+                                  username=device_parameters[self.__device_username],
+                                  password=device_parameters[self.__device_password], timeout=self.__device_timeout)
+            logging.info(f'Connection to device {device} established on {device_parameters[self.__device_ipaddress]}:{device_parameters[self.__device_port]}')
+            for filename_suffix in device_command_set.keys():
+                backup_filename = self.create_filename(device, filename_suffix)
+                file_stamp = self.create_filestamp(device)
+                self.write_data_to_file(file_stamp, backup_filename)
+                logging.debug(f'File stamp: {file_stamp} written to backup file: {backup_filename}')
+                for command in device_command_set[filename_suffix]:
+                    command_header = self.create_command_header(command)
+                    self.write_data_to_file(command_header, backup_filename)
+                    logging.debug(f'File command header: {command_header} written to backup file: {backup_filename}')
+                    stdin, stdout, stderr = devconnection.exec_command(command)
+                    logging.debug(f'Data for command: {command} downloaded from device: {device}')
+                    device_textdata = self.convert_listdata_to_textdata(stdout.readlines())
+                    self.write_data_to_file(device_textdata, backup_filename)
+                    logging.info(f'Data written to file: {backup_filename}')
+            devconnection.close()
+            logging.info(f'Connection to device: {device} closed')
+        except paramiko.BadHostKeyException as bhke:
+            logging.error(f'Connection error occurred for device: {device}. Server’s host key could not be verified', exc_info=True)
+            print(f'\nWYSTĄPIŁ BŁĄD W TRAKCIE POŁĄCZENIA Z URZĄDZENIEM {device}!!!'
+                  f'Nie można zweryfikować klucza hosta.\n', bhke)
+            print('Sprawdź log programu, aby poznać szczegóły.')
+        except paramiko.AuthenticationException as ae:
+            logging.error(f'Connection error occurred for device: {device}. Authentication failed', exc_info=True)
+            print(f'\nWYSTĄPIŁ BŁĄD W TRAKCIE POŁĄCZENIA Z URZĄDZENIEM {device}!!!'
+                  f'Autentykacja zakończona niepowodzeniem.\n', ae)
+            print('Sprawdź log programu, aby poznać szczegóły.')
+        except paramiko.SSHException as sshe:
+            logging.error(f'Connection error occurred for device: {device}. Connecting or establishing an SSH session failed', exc_info=True)
+            print(f'\nWYSTĄPIŁ BŁĄD W TRAKCIE POŁĄCZENIA Z URZĄDZENIEM {device}!!!'
+                  f'Próba połączenia lub ustanawiania sesji SSH zakończona niepowowdzeniem.\n', sshe)
+            print('Sprawdź log programu, aby poznać szczegóły.')
+        except:
+            logging.error(f'Connection error occurred for device: {device}', exc_info=True)
+            print(f'\nWYSTĄPIŁ BŁĄD W TRAKCIE POŁĄCZENIA Z URZĄDZENIEM {device}!!!')
+            print('Sprawdź log programu, aby poznać szczegóły.')
+
+    def convert_listdata_to_textdata(self, listdata):
+        """Przekształcenie danych pobranych z urządzenia w formacie listy na dane w postaci tekstu - linia po linii."""
+        textdata = ''
+        for position in listdata:
+            textdata += position
+        logging.debug(f'Listdata has been converted to textdata')
+        return textdata
+
+    def get_backup_dirpath(self, config_filename):
+        """Pobranie ścieżki do katalogu przeznaczonego na pliki z backup-ami z pliku konfiguracyjnego."""
+        config = configparser.ConfigParser()
+        config.read(config_filename)
+        backup_dirpath = config[self.__GENERAL_PARAMETERS][self.__bkpfiles_directory]
+        logging.debug(f'Backup directory path created: {backup_dirpath}')
+        return backup_dirpath
+
+    def create_filename(self, device_name, suffix):
+        """Utworzenie nazwy pliku backup-owego dla danych pobranych z urządzenia na podstawie nazwy urządzenia (device_name) oraz przyrostka (suffix) nadawanego
+        na podstawie pliku konfiguracyjnego - nazwa opcji w sekcji stanowiącej zbiór komend dla danej grupy urządzeń."""
+        filename = device_name + '_' + suffix + '.txt'
+        logging.debug(f'Backup filename created: {filename}')
+        return filename
+
+    def write_data_to_file(self, data, filename):
+        """Zapis danych do pliku."""
+        config = configparser.ConfigParser()
+        config.read(self.CONFIG_FILENAME)
+        filepath = Path(self.get_backup_dirpath(self.CONFIG_FILENAME)) / filename
+        with open(filepath, mode='a') as bkpfile:
+            bkpfile.write(data)
+        logging.debug(f'Data written to filepath: {filepath}')
+        return 0
+
+    def get_current_date(self):
+        '''Pobiera aktualną datę i godzinę oraz zwraca w czytelnej postaci.'''
+        current_date = datetime.now()
+        formatted_current_date = current_date.strftime('%d-%m-%Y %H:%M:%S')
+        logging.debug(f'Formatted current date created as {formatted_current_date}')
+        return formatted_current_date
+
+    def create_filestamp(self, device_name):
+        """Utworzenie odcisku dla pliku."""
+        time_signature = self.get_current_date()
+        filestamp = 10*'*' + ' FILESTAMP ' + 10*'*' + '\nDevice Name: ' + device_name + '\nCreated: ' + time_signature + '\n' + 31*'*' + '\n'
+        logging.debug('Filestamp created')
+        return filestamp
+
+    def create_command_header(self, command):
+        """Utworzenie nagłówka w pliku backup-owym dla danych wynikowych wykonania wskazanej komendy."""
+        command_header = '\n\n' + 40*'=' + ' ' + command + ' ' + 40*'=' + '\n\n'
+        logging.debug(f'Command header for command: {command} created')
+        return command_header
